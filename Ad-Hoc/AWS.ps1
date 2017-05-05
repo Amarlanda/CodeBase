fdf8d6e3a7550984818aff4c6aa38d9f66a847fb
Amar Landa <AL@amarlanda.com>
2017-05-05 17:01:20 +0100

    Add files via upload

---
+++ Ad-Hoc/AWS.ps1
@@ -0,0 +1,7 @@
+﻿Get-EC2Image -Owners amazon -Filters @{Name = 'name'
+ Values = 'Windows_Server-2012-R2*English*'} | Select-Object -Property imageid,name | Format-Table -AutoSize
+
+$AMI = 'ami-dd9eb6ae'
+
+new-EC2Instance -ImageId $AMI -MinCount 1 -MaxCount 1 -KeyName PuppetAccess -SecurityGroups default -InstanceType t2.micro 
+
---
+++ Ad-Hoc/git.ps1
@@ -0,0 +1,42 @@
+﻿$url =  "https://api.github.com"
+
+$endpoint = "/gists"
+
+$urlAnon = "$url" + "$endpoint"
+
+
+$JSON2 = ConvertTo-Json @{
+  description  = "the description for this gists";
+  public = $true;
+  files = @{
+    "file1.txt" =@{
+      content = "String file contents"
+    
+    }
+
+  }
+
+}
+
+
+
+$token = "92e97d33ab19dab30da23b11624cc1e0a4887585"
+
+$urlSecure = "$urlAnon" + "?access_token=" + "$token"
+$gists = Invoke-RestMethod -Method Post -Uri $urlSecure -Body $JSON2
+
+
+$gists 
+
+$JSON2 = ConvertTo-Json @{
+  description  = "monkey fish";
+  public = $true;
+  files = @{
+    "file1.txt" =@{
+      content = "im tired"
+    
+    }
+
+  }
+
+}
