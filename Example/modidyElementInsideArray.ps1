8102c96c179ac7a981744217ebb98aee71f8dcf0
Amar Landa <al@amarlanda.com>
2017-11-02 12:33:24 +0000

    automated push 11/02/2017 12:33:24

---
+++ Example/modidyElementInsideArray.ps1
@@ -0,0 +1,10 @@
+﻿#More detials here
+#https://stackoverflow.com/questions/34166023/powershell-modify-elements-of-array
+
+$myArray = 1,2,3,4,5
+$myArray = $myArray |ForEach-Object {
+  $_ *= 10
+  $_
+}
+
+$myArray
